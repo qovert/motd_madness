fa9503f899dbcae126a530286ae5a9a99ce71801
Brian Harkreader <qovert@users.noreply.github.com>
2020-10-03 20:34:02 -0700

    Create pihole.sh

---
+++ pihole.sh
@@ -0,0 +1,13 @@
+Welcome to $(hostname), $(whoami)!
+
+   .~~.   .~~.       `date +"%A, %e %B %Y, %r"`
+  '. \ ' ' / .'      Uptime: $(uptime -p)$(tput setaf 1)
+   .~ .~~~..~.
+  : .~.'~'.~. :      Memory: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kb (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kb (Total)
+ ~ (   ) (   ) ~     Load Averages......: $(uptime | grep -oP '(?<=average: ).*')
+( : '~'.~.'~' : )    Running Processes..: `ps ax | wc -l | tr -d " "`
+ ~ .~ (   ) ~. ~     IP Addresses.......: `ip -4 addr show eth0 |grep inet | awk {'print $2'} |cut -d "/" -f 1` Ext: `curl -s http://icanhazip.com`
+  (  : '~' :  )      Disk Space.........: Free: `df -Ph | grep -E 'tank/apps/subvol-110-disk-0' | awk '{ print $4 }'` | Used: `df -Ph | grep -E '^tank/apps/subvol-110-disk-0' | awk '{ print $3 }'` $(tput setaf 2)`df -Ph | grep -E '^tank/apps/subvol-110-disk-0' | awk '{ print $5 }'`$(tput setaf 1)
+   '~ .~~~. ~'       Current `curl -s https://v2.wttr.in/98115\?0 |grep "Weather:"`
+       '~'
+$(tput sgr0)"
